9dc588e63e1dec2a09cdf0eba569319ea29cb38b
Lloyd Farrell <lloyd.farrell@go.sfcollege.edu>
2025-09-01 11:19:16 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+--kill aura loader
+loadstring(game:HttpGet("https://github.com/LloydFarrelSFCollege/V_FLY/blob/main/killaura.lua"))()
+--fly loader
+loadstring(game:HttpGet("hthttps://github.com/LloydFarrelSFCollege/V_FLY/blob/main/FLY.lua"))()
+--autopickup loader
+loadstring(game:HttpGet("https://github.com/LloydFarrelSFCollege/V_FLY/blob/main/autopickup.lua"))()
+--godmode loader
+loadstring(game:HttpGet("https://github.com/LloydFarrelSFCollege/V_FLY/blob/main/god%20mode.lua"))()
